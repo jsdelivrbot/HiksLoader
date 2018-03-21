8ce7ce60b0fa6c0d331429c809475d505ae3e5b6
Hiki Hollow <imh1kiko@gmail.com>
2018-03-21 15:24:03 +0200

    Final tweaks.

--- MsgBackground.js
+++ MsgBackground.js
@@ -8,20 +8,26 @@ var closestParentBot = $('strong[style="color: rgb(31, 139, 76);"]').closest(".m
 		--- CODE ---
 ***/
 
-	$(closestParentBot).find(".accessory:not(:empty)")
-		.css({
-			background: "url(https://pa1.narvii.com/5579/b6a9020b6bf1efbda206edaa547ea8cc432739e8_hq.gif) 0 0 / cover no-repeat",
-			padding: "5px",
-			margin: "5px",
-			border: "1px solid rgba(210, 42, 150, 0.55)",
-			"border-radius": "10px"
-		});
+$(closestParentBot).find("[class*='embedInner']:not(:empty)")
+	.css({
+		background: "url(https://pa1.narvii.com/5579/b6a9020b6bf1efbda206edaa547ea8cc432739e8_hq.gif) 0 0 / cover no-repeat",
+		padding: "5px",
+		margin: "5px",
+		border: "1px solid rgba(210, 42, 150, 0.55)",
+		"border-radius": "10px"
+	});
 
-	$(closestParentBot).find(".message-text:not(:has(.markup:empty))")
-		.css({
-			background: "url(https://pa1.narvii.com/5579/b6a9020b6bf1efbda206edaa547ea8cc432739e8_hq.gif) 0 0 / cover no-repeat",
-			padding: "5px",
-			margin: "5px",
-			border: "1px solid rgba(210, 42, 150, 0.55)",
-			"border-radius": "10px"
-		});
+$(closestParentBot).find(".message-text:not(:has(.markup:empty))")
+	.css({
+		background: "url(https://pa1.narvii.com/5579/b6a9020b6bf1efbda206edaa547ea8cc432739e8_hq.gif) 0 0 / cover no-repeat",
+		padding: "5px",
+		margin: "5px",
+		border: "1px solid rgba(210, 42, 150, 0.55)",
+		"border-radius": "10px",
+	});
+
+$(closestParentBot).find("[class*='embedInner-']:not(:empty), [class*='embedFooterText-'], .markup:not(:empty)").css({
+	"background-color": "rgba(0,0,0,0.5)",
+	"border-radius": "10px",
+	padding: "5px"
+})
